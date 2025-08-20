335d3e961ff6092d7d0ff31c8935e7e8ed6f0073
Andrew <andrew@victus>
2025-08-20 22:27:18 +0100

    added Nav Bar spacing and hover effects, updated main.css with theme colors.

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <router-view />
 </template>
 
 <script setup lang="ts">
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/main.css'
+
+//Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 
 const app = createApp(App)
 
 app.use(router)
+
+library.add(fas)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
